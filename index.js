109fc9d00aa8fcafc82fa71e2ce6208064cfcfea
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-06-03 00:08:29 +0600

    post added

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const app = express();
 const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
-const port = process.env.PORT;
+const port = 5000;
 
 app.use(cors());
 app.use(express.json());
-
 
 const uri =
   "mongodb+srv://blooming-elegance:6mJs5Ez8KDbDI3Vx@cluster0.guksi.mongodb.net/blooming-elegance?retryWrites=true&w=majority&appName=Cluster0";
@@ -24,9 +23,10 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const productDB = client.db("productDB");
+    const bloomingElegance = client.db("bloomingElegance");
     const userDB = client.db("userDB");
-    const flowersCollection = productDB.collection("flowersCollection");
+    const flowersCollection =
+      bloomingElegance.collection("flowersCollection");
     const userCollection = userDB.collection("userCollection");
 
     // product
@@ -42,7 +42,31 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/flowers/:id", async (req, res) => {
+      const id = req.params.id;
+      const flowersData = await flowersCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send(flowersData);
+    });
 
+    app.patch("/flowers/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedData = req.body;
+      const result = await flowersCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedData }
+      );
+      res.send(result);
+    });
+
+    app.delete("/flowers/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await flowersCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
 
     // user
     app.post("/user", async (req, res) => {
@@ -52,7 +76,6 @@ async function run() {
     });
 
     // user/test@gmail
-    
 
     console.log("Database is connected");
   } finally {
