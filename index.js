7473bee30ffa21c5e12274c0735220d9e6561b73
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-06-03 23:34:18 +0600

    locally handled

--- index.js
+++ index.js
@@ -2,47 +2,51 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
-const { MongoClient, ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+function createToken(user) {
+  const token = jwt.sign(
+    {
+      email: user.email,
+    },
+    "secret",
+    { expiresIn: "7d" }
+  );
+  return token;
+}
 
-const createToken = (user) => {
-  return jwt.sign({ email: user.email }, "secret", { expiresIn: "7d" });
-};
-
-const verifyToken = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) return res.status(401).send("Unauthorized access");
-  const token = authHeader.split(" ")[1];
-  try {
-    const verified = jwt.verify(token, "secret");
-    req.user = verified.email;
-    next();
-  } catch (error) {
-    res.status(401).send("Unauthorized access");
+function verifyToken(req, res, next) {
+  const token = req.headers.authorization.split(" ")[1];
+  const verify = jwt.verify(token, "secret");
+  if (!verify?.email) {
+    return res.send("You are not authorized");
   }
-};
+  req.user = verify.email;
+  next();
+}
 
-const isValidObjectId = (id) => {
-  return ObjectId.isValid(id) && String(new ObjectId(id)) === id;
-};
+const uri = process.env.DATABASE_URL;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 async function run() {
   try {
     await client.connect();
-    const db = client.db("bloomingElegance");
-    const flowersCollection = db.collection("flowersCollection");
-    const userCollection = db.collection("userCollection");
-
+    const bloomingElegance = client.db("bloomingElegance");
+    const flowersCollection = bloomingElegance.collection("flowersCollection");
+    const userCollection = bloomingElegance.collection("userCollection");
+    // product
     app.post("/flowers", verifyToken, async (req, res) => {
       const flowersData = req.body;
       const result = await flowersCollection.insertOne(flowersData);
@@ -50,24 +54,20 @@ async function run() {
     });
 
     app.get("/flowers", async (req, res) => {
-      const flowersData = await flowersCollection.find().toArray();
-      res.send(flowersData);
+      const flowersData = flowersCollection.find();
+      const result = await flowersData.toArray();
+      res.send(result);
     });
 
     app.get("/flowers/:id", async (req, res) => {
       const id = req.params.id;
-      if (!isValidObjectId(id)) {
-        return res.status(400).send({ error: "Invalid ObjectId format" });
-      }
-      const flower = await flowersCollection.findOne({ _id: new ObjectId(id) });
-      res.send(flower);
+      const flowersData = await flowersCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send(flowersData);
     });
-
     app.patch("/flowers/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
-      if (!isValidObjectId(id)) {
-        return res.status(400).send({ error: "Invalid ObjectId format" });
-      }
       const updatedData = req.body;
       const result = await flowersCollection.updateOne(
         { _id: new ObjectId(id) },
@@ -75,71 +75,66 @@ async function run() {
       );
       res.send(result);
     });
-
     app.delete("/flowers/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
-      if (!isValidObjectId(id)) {
-        return res.status(400).send({ error: "Invalid ObjectId format" });
-      }
       const result = await flowersCollection.deleteOne({
         _id: new ObjectId(id),
       });
       res.send(result);
     });
-
+    // user
     app.post("/user", async (req, res) => {
       const user = req.body;
+
       const token = createToken(user);
-      const isUserExist = await userCollection.findOne({ email: user.email });
-      if (isUserExist) {
+      const isUserExist = await userCollection.findOne({ email: user?.email });
+      if (isUserExist?._id) {
         return res.send({
-          status: "success",
-          message: "Login successful",
+          statu: "success",
+          message: "Login success",
           token,
         });
       }
       await userCollection.insertOne(user);
-      res.send({ token });
+      return res.send({ token });
     });
+
+    // user/test@gmail
 
     app.get("/user/get/:id", async (req, res) => {
       const id = req.params.id;
-      if (!isValidObjectId(id)) {
-        return res.status(400).send({ error: "Invalid ObjectId format" });
-      }
-      const user = await userCollection.findOne({ _id: new ObjectId(id) });
-      res.send(user);
+      console.log(id);
+      const result = await userCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
     });
 
     app.get("/user/:email", async (req, res) => {
       const email = req.params.email;
-      const user = await userCollection.findOne({ email });
-      res.send(user);
+      const result = await userCollection.findOne({ email });
+      res.send(result);
     });
 
     app.patch("/user/:email", async (req, res) => {
       const email = req.params.email;
-      const updatedData = req.body;
+      const userData = req.body;
       const result = await userCollection.updateOne(
         { email },
-        { $set: updatedData },
+        { $set: userData },
         { upsert: true }
       );
       res.send(result);
     });
 
-    console.log("Connected to database");
-  } catch (err) {
-    console.error(err);
+    console.log("Database is connected");
+  } finally {
   }
 }
-
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("API is working");
+  res.send("Route is working");
 });
 
-app.listen(port, () => {
-  console.log(`App is listening on port ${port}`);
+app.listen(port, (req, res) => {
+  console.log("App is listening on port :", port);
 });
